59e85fd5800b903a5f20db0ef3a5dd9937244e10
Michael Farber Brodsky <taco@mailo.com>
2021-02-25 00:49:42 +0200

    Add example to main

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
-#define TRACKLLOC
 #include "tracklloc.h"
 
 int main() {
     int *x = malloc(sizeof(int));
     printf("x is %i at %p\n", *x, x);
+    free(x);
+    // can't detect unfreed memory in files that don't include tracklloc
+    char *s = strdup("hello world");
+    // if we'd free s it says it's double freeing
     TRACK_log_unfreed();
 } 
